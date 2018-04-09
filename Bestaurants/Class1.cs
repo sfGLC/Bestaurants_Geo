16e6e69ccd76ce30ecb64ae31b1db19b3119ba5a
Siyu Fan <siyu.fan@int.geo-nexus.com>
2018-04-09 01:15:16 -0400

    Initial commit

---
+++ Bestaurants/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Bestaurants
+{
+    [Guid("cfff608e-0206-4b38-9e79-9d831048809e")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("Bestaurants.Class1")]
+    public class Class1
+    {
+    }
+}
---
+++ Bestaurants/cmdLoadData.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.ArcMapUI;
+
+namespace Bestaurants
+{
+    /// <summary>
+    /// Summary description for cmdLoadData.
+    /// </summary>
+    [Guid("bf952b08-614f-4264-98f1-547473fb1113")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("Bestaurants.cmdLoadData")]
+    public sealed class cmdLoadData : BaseCommand
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+
+        }
+
+        #endregion
+        #endregion
+
+        private IApplication m_application;
+        public cmdLoadData()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "Bestaurants_Tools"; //localizable text
+            base.m_caption = "Load Data";  //localizable text
+            base.m_message = "Load data into ArcMap from external files";  //localizable text 
+            base.m_toolTip = "Load Data";  //localizable text 
+            base.m_name = "Bestaurants_Tools_LoadData";   //unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+
+            try
+            {
+                //
+                // TODO: change bitmap name if necessary
+                //
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this command is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            if (hook == null)
+                return;
+
+            m_application = hook as IApplication;
+
+            //Disable if it is not ArcMap
+            if (hook is IMxApplication)
+                base.m_enabled = true;
+            else
+                base.m_enabled = false;
+
+            // TODO:  Add other initialization code
+        }
+
+        /// <summary>
+        /// Occurs when this command is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            // TODO: Add cmdLoadData.OnClick implementation
+        }
+
+        #endregion
+    }
+}
